73d7c3b855f7bb1161a995a121871c68ee81e64c
KaMa75 <karolmarszalek@o2.pl>
2020-10-29 17:21:05 +0100

    add and implement Button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+
+import Button from './components/Button';
+
 import './App.css';
 
 function App() {
   return (
-    <div>test</div>
+    <Button>Click me!</Button>
   );
 }
 
