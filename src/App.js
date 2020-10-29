43e1e7d8024920ce1346e7e24ec86380429c6670
KaMa75 <karolmarszalek@o2.pl>
2020-10-29 18:45:25 +0100

    Button component with default props and awesome icon

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import Button from './components/Button';
 
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+library.add(faUser);
+
+
+const colors = {
+  fadedPoster: '#81ecec',
+  americanRiver:'#636e72'
+}
+
 function App() {
+  const {fadedPoster, americanRiver} = colors;
   return (
-    <Button>Click me!</Button>
+    <Button
+      icon='user'
+      bgColor={fadedPoster}
+      color={americanRiver}
+    >
+      Click me!
+    </Button>
   );
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Button({children}) {
+function Button({children, bgColor, color, icon}) {
+    const styles = {
+        color: color,
+        backgroundColor: bgColor
+    }
+
     return (
-    <button>{children}</button>
+        <button
+            style={styles}
+        >
+            <FontAwesomeIcon icon={icon} />
+            <span>{children}</span>
+        </button>
     );
+}
+
+Button.defaultProps = {
+    bgColor: '#dfe6e9',
+    color: '#6c5ce7'
 }
 
 export default Button;
