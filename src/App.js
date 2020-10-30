42da7d6fd9e2c266ae56f5faedbfe42bc8f320e8
KaMa75 <karolmarszalek@o2.pl>
2020-10-30 20:41:11 +0100

    add cross-env dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Form from './components/Form';
+import Form from 'components/Form';
 import './App.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
